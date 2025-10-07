51fe41211eb4c7a71c61928e8f3e9a01bcaab65a
Tigran Ghumashyan <tigranghumashyan@gmail.com>
2025-10-07 20:36:11 +0400

    Fix

--- src/shared/ui/EmptyState.tsx
+++
@@ -1,9 +0,0 @@
-import { Empty } from 'antd'
-
-interface EmptyStateProps {
-  description?: string
-}
-
-export function EmptyState({ description = 'Нет данных' }: EmptyStateProps) {
-  return <Empty description={description} />
-}
